35d83d5bcc4f7e21cf66e5842bbe050bcb0aecfb
mqtias <matiasjamsen2@gmail.com>
2026-09-25 11:06:13 +0300

    muokkaa error_handleria

--- error_handler.py
+++ error_handler.py
@@ -1,11 +1,6 @@
-"""
-Keskitetty virheenkäsittelyohjelma.
-"""
-
 import datetime
 
 def kasittele_virhe(virhe, lisatieto=""):
-    """Tallentaa virheen lokitiedostoon ja tulostaa sen käyttäjälle."""
     aikaleima = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     virhe_viesti = f"[{aikaleima}] VIRHE: {lisatieto} -> {str(virhe)}\n"
     
